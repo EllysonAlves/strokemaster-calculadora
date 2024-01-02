a7f215781b941c6d0c7016dab1445e2f62f7691c
Ellyson Alves <ellyson.alves.ds@gmail.com>
2024-01-02 02:22:31 -0300

    v1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@
 import styles from './index.module.css';
 import * as React from 'react';
 import InputLabel from '@mui/material/InputLabel';
-
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
